a05df53d726533c500a0f5d3ac1acb52ed4be1a5
the-sidh <41924052+the-sidh@users.noreply.github.com>
2022-10-22 15:17:58 -0300

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,58 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("maven-publish")
+    id("signing")
 }
 
-group = "me.sid"
-version = "1.0-SNAPSHOT"
+group = "io.github.the_sidh"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
+
+kotlin {
+    sourceSets {
+        val main by getting {
+            dependencies {
+                implementation("com.github.the_sidh:kobbit:1.0.0")
+            }
+        }
+    }
+}
+
+val sonatypeUsername: String? = System.getenv("SONATYPE_USERNAME")
+val sonatypePassword: String? = System.getenv("SONATYPE_PASSWORD")
+val javadocJar = tasks.register("javadocJar", Jar::class.java) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        repositories {
+            maven {
+                name="oss"
+                val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
+            }
+        }
+        publications {
+            withType<MavenPublication> {
+                artifact(javadocJar)
+                pom { /* ... */ }
+            }
+        }
+    }
+}
+
+
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
